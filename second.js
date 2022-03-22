2bf95e8586ec7b92830044c7f7e5f3af253caf00
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2022-03-23 05:03:16 +0530

    add all files

---
+++ second.js
@@ -0,0 +1,54 @@
+var a = true
+var b = true
+
+true+true
+2
+true+false
+1
+
+true is equal to 1
+false is equal to 0
+
+true+false
+1
+10+true
+11
+"a"+true
+atrue
+"a"+true+true
+atruetrue
+true+true+"a"
+"2a"
+10/false
+Infinity
+var a="10.52"
+parseInt(a)
+10
+
+var a = "hi"
+parseInt(a)
+NaN
+
+var a = "11a2"
+var b = "12a"
+var c = "a12"
+
+parseInt(a)
+11
+parseInt(b)
+12
+parseInt(c)
+NaN
+
+var a = 10
+var b = "hi"
+var c = true(1)
+
+isNaN(a)(is not a Number)
+false
+
+isNaN(b)
+true
+
+isNaN(c)
+false
