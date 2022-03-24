cff633940daf197fb13d7d66cc7a6d88d39a0a1f
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2022-03-24 12:06:31 +0530

    add all files

---
+++ stringOperation.js
@@ -0,0 +1,32 @@
+var url = "https://docs.microsoft.com/en-us/office/dev/add-ins/excel/";
+
+var a = "javascript"
+
+var ur= "https://docs.microsoft.com/en-us/office/dev/add-ins/excel/"
+undefined
+url.split('/')
+VM123:1 Uncaught ReferenceError: url is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM123:1
+var url = "https://docs.microsoft.com/en-us/office/dev/add-ins/excel/"
+undefined
+url.split('/')
+(9) ['https:', '', 'docs.microsoft.com', 'en-us', 'office', 'dev', 'add-ins', 'excel', '']0: "https:"1: ""2: "docs.microsoft.com"3: "en-us"4: "office"5: "dev"6: "add-ins"7: "excel"8: ""length: 9[[Prototype]]: Array(0)
+out[out.length-1]
+VM254:1 Uncaught ReferenceError: out is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM254:1
+out[0]
+VM273:1 Uncaught ReferenceError: out is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM273:1
+var out = url.split('/')
+undefined
+out[0]
+'https:'
+out[out.length-1]
+''
+var a = "javascript"
+undefined
+a.split('')
+(10) ['j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
